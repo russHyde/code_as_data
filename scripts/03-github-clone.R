c7cd466774cadd5f0ee911a78e8a61de15b9eca1
Russell Hyde <russ.hyde.data@gmail.com>
2020-01-28 10:16:31 +0000

    rewrite github cloning script to work on a single repo
    Add:
    - `--local_repo` and `--remote_repo` arguments so that the script is
    called with explicit intent: clone this to here.

--- scripts/03-github-clone.R
+++ scripts/03-github-clone.R
@@ -1,8 +1,8 @@
 ###############################################################################
 
 # pkgs required for running the script
-general_pkgs <- c("here", "magrittr", "optparse", "yaml")
-pkgs <- c(general_pkgs, "dplyr", "git2r", "purrr", "readr")
+general_pkgs <- c("here", "magrittr", "optparse")
+pkgs <- c(general_pkgs, "git2r")
 
 for (pkg in pkgs) {
   suppressPackageStartupMessages(
@@ -14,63 +14,48 @@ for (pkg in pkgs) {
 
 define_parser <- function() {
   description <- paste(
-    "Download lots of R packages from a remote to a local location.",
-    "For each package mentioned in a file, this downloads the github repo",
-    "from a remote to a local location."
+    "Clones an R packages from a (github) remote to a local location.",
+    "(Admittedly this is trivial)"
   )
 
   parser <- OptionParser(
     description = description
   ) %>%
     add_option(
-      c("--config"), type = "character",
+      "--remote_repo", type = "character",
       help = paste(
-        "A .yaml file containing the configuration details for the workflow.",
-        "The .yaml should contain fields for the keys:",
-        "- 'repo_details_file' (package,remote_repo,local_repo)."
+        "The URL for a github repository that is to be cloned"
+      )
+    ) %>%
+    add_option(
+      "--local_repo", type = "character",
+      help = paste(
+        "The filepath for the directory where the cloned repository should be",
+        "placed"
       )
     )
 }
 
 ###############################################################################
 
-clone_repositories <- function(x) {
-  # Clone each package from its remote to its local repo (but only if we
-  # haven't already cloned it)
-  purrr::walk2(
-    x$remote_repo, x$local_repo, git2r::clone
-  )
+main <- function(
+  remote_repo, local_repo
+) {
+  # Takes a remote URL and a local filepath, then clones the (presumed github
+  # URL) from the remote_repo location to the local_repo directory
+
+  if (!dir.exists(local_repo)) {
+    git2r::clone(remote_repo, local_repo)
+  }
 }
 
 ###############################################################################
-
-main <- function(repo_details_file) {
-  # Takes a table of CRAN packages of the form (package-name, remote-repo,
-  # local-repo) and clones each package from it's remote location to the
-  # specified local location.
-
-  repo_details <- read_repo_details(repo_details_file)
-
-  # Columns of `repo_details`:
-  # (package, remote_repo, local_repo)
-  stopifnot(
-    all(c("package", "remote_repo", "local_repo") %in% colnames(repo_details))
-  )
-
-  repo_details %>%
-    dplyr::filter(!dir.exists(local_repo)) %>%
-    clone_repositories()
-}
-
-###############################################################################
-
-source(here("scripts", "utils.R"))
 
 opt <- optparse::parse_args(define_parser())
-config <- yaml::read_yaml(opt$config)
 
 main(
-  repo_details_file = here(config[["repo_details_file"]])
+  remote_repo = opt$remote_repo,
+  local_repo = opt$local_repo
 )
 
 ###############################################################################
