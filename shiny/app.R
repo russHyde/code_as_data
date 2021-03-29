f98894098c5c6837390fd292b50a13037cec2457
Russ Hyde <russ.hyde.data@gmail.com>
2021-03-29 11:02:30 +0100

    app: lets user choose which summary statistic to display (loc, n_contrib, ...)

--- shiny/app.R
+++ shiny/app.R
@@ -16,14 +16,28 @@ files <- list(
   gitsum = file.path(dirs[["app_data"]], "dev-pkg-gitsum.tsv")
 )
 
+# TODO: fix the y-axis label of the barplot to use the label in this vector
+pkg_statistics <- c(
+  "Lines of Code" = "loc",
+  "Number of Commits" = "n_commits",
+  "Number of Contributors" = "n_contributors"
+)
+
 # Helper functions
 
+#' Takes the package-summarised dataset and produces a barplot, with packages
+#' ordered by a user-selected statistic (lines of code, number of commits
+#' etc)
+#' @param df   The data-frame. This should contain columns (package, n_commits,
+#' n_contributors, loc, ...)
+#' @param column   String. Which of the columns within the dataframe should a
+#'   barplot be made for?
 barplot_by_package <- function(df, column) {
-  # takes the package-summarised dataset and produces a barplot, with packages
-  # ordered by a user-selected statistic (lines of code, number of commits etc)
   df %>%
-    mutate(package = forcats::fct_reorder(package, {{ column }}, .desc = TRUE)) %>%
-    ggplot(aes(x = package, y = {{ column }})) +
+    mutate(
+      package = forcats::fct_reorder(package, .data[[column]], .desc = TRUE)
+    ) %>%
+    ggplot(aes(x = package, y = .data[[column]])) +
     geom_bar(stat = "identity") +
     guides(x = guide_axis(angle = 90))
 }
@@ -33,7 +47,16 @@ barplot_by_package <- function(df, column) {
 ui <- fluidPage(
   titlePanel("Code as data"),
   dataTableOutput("package_summary_table"),
-  plotOutput("pkg_summary_barplot"),
+  fluidRow(
+    column(
+      3,
+      selectInput(
+        "chosen_stat",
+        "Choose a statistic to display",
+        choices = pkg_statistics)
+    ),
+    column(9, plotOutput("pkg_summary_barplot"))
+  ),
   footer()
 )
 
@@ -56,11 +79,9 @@ server <- function(input, output, session) {
     options = list(pageLength = 5)
   )
 
-  # TODO: allow user to choose whether to make barplot of: loc, n_commits,
-  # n_contributors etc
   output$pkg_summary_barplot <- renderPlot(
     data_by_package() %>%
-      barplot_by_package(loc)
+      barplot_by_package(input$chosen_stat)
   )
 }
 
